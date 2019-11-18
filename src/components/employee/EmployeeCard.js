2acbcee55905bf1d5d8092907ee3c728273b2b55
Ryan Crowley <ryancrowleycode@gmail.com>
2019-11-18 11:34:01 -0600

    added alt tag to david's picture

--- src/components/employee/EmployeeCard.js
+++ src/components/employee/EmployeeCard.js
@@ -7,7 +7,7 @@ class EmployeeCard extends Component {
                 <div className="card-content">
                     <h3>Employee</h3>
                     <picture>
-                        <img src={require('./david-dogperson.jpg')} />
+                        <img src={require('./david-dogperson.jpg')} alt="David Dogperson and his dog."/>
                     </picture>
                     <h4>David Dogperson</h4>
                     <p>We are happy to have David on our team! David moved to Nashville in 2017 from North Carolina, where he helped to raise his family's 9 dogs.</p>
