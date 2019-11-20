6c5c69d612caf2116da3f6e40d891c0df0e75976
Ryan Crowley <ryancrowleycode@gmail.com>
2019-11-20 09:14:40 -0600

    added delete to ApiManager and modified to work with any category

--- src/modules/ApiManager.js
+++ src/modules/ApiManager.js
@@ -6,5 +6,12 @@ export default {
   },
   getAll(category) {
     return fetch(`${remoteURL}/${category}`).then(result => result.json())
+  },
+
+  delete(id, category) {
+    return fetch(`http://localhost:5002/${category}/${id}`, {
+        method: "DELETE"
+    })
+    .then(result => result.json())
   }
 }
