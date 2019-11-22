c3924db7b00c813b3c8a63aa66231f46d4861461
Ryan Crowley <ryancrowleycode@gmail.com>
2019-11-22 13:35:11 -0600

    EmployeeEditForm.js is completed

--- src/components/employee/EmployeeEditForm.js
+++ src/components/employee/EmployeeEditForm.js
@@ -18,7 +18,7 @@ class EmployeeEditForm extends Component {
 
     updateExistingEmployee = evt => {
         evt.preventDefault()
-        this.setState({ loadingStatus: true})
+        this.setState({ loadingStatus: true })
         const editedEmployee = {
             id: this.props.match.params.employeeId,
             name: this.state.employeeName,
@@ -31,13 +31,56 @@ class EmployeeEditForm extends Component {
 
     componentDidMount() {
         ApiManager.get(this.props.match.params.employeeId, "employees")
-        .then(employee => {
-            this.setState({
-                employeeName: employee.name,
-                employeeInfo: employee.info,
-                loadingStatus: false
+            .then(employee => {
+                this.setState({
+                    employeeName: employee.name,
+                    employeeInfo: employee.info,
+                    loadingStatus: false
+                })
             })
-        })
+    }
+
+    render() {
+        return (
+            <>
+                <form>
+                    <fieldset>
+                        <div className="formgrid">
+                            <input
+                                type="text"
+                                required
+                                className="form-control"
+                                onChange={this.handleFieldChange}
+                                id="employeeName"
+                                value={this.state.employeeName}
+                            />
+                            <label htmlFor="employeeName">
+                                Employee name
+                            </label>
+
+                            <input
+                                type="text"
+                                required
+                                onChange={this.handleFieldChange}
+                                id="employeeInfo"
+                                value={this.state.employeeInfo}
+                            />
+                            <label htmlFor="employeeInfo">
+                                Employee info
+                            </label>
+                        </div>
+                        <div className="alignRight">
+                            <button
+                                type="button"
+                                disabled={this.state.loadingStatus}
+                                onClick={this.updateExistingEmployee}
+                                className="btn btn-primary"
+                            >Submit</button>
+                        </div>
+                    </fieldset>
+                </form>
+            </>
+        )
     }
 
 }
