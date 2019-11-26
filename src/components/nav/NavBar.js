797e693e4c8abf98a9d3837643133130bf4b789c
Ryan Crowley <ryancrowleycode@gmail.com>
2019-11-26 10:40:05 -0600

    logout is functioning

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -3,9 +3,13 @@ import { Link, withRouter } from "react-router-dom"
 import './NavBar.css'
 
 class NavBar extends Component {
-  
 
-  render(){
+  handleLogout = () => {
+    this.props.clearUser()
+    this.props.history.push('/')
+  }
+
+  render() {
 
     return (
       <header>
@@ -15,11 +19,18 @@ class NavBar extends Component {
         <nav>
           <ul className="container">
             <li><Link className="nav-link" to="/">Home</Link></li>
-            {(this.props.user) ? <li><Link className="nav-link" to="/animals">Animals</Link></li> : null}
             <li><Link className="nav-link" to="/locations">Locations</Link></li>
-            {(this.props.user) ? <li><Link className="nav-link" to="/employees">Employees</Link></li> : null }
-            {(this.props.user) ? <li><Link className="nav-link" to="/owners">Owners</Link></li> : null }
-            <li><Link className="nav-link" to="/login">Login</Link></li>
+            {(this.props.user) ?
+              <React.Fragment>
+                <li><Link className="nav-link" to="/animals">Animals</Link></li>
+                <li><Link className="nav-link" to="/employees">Employees</Link></li>
+                <li><Link className="nav-link" to="/owners">Owners</Link></li>
+                <li><span className="nav-link" onClick={this.handleLogout}>Logout</span></li>
+              </React.Fragment>
+              :
+              <li><Link className="nav-link" to="/login">Login</Link></li>
+            }
+
           </ul>
         </nav>
       </header>
@@ -27,4 +38,4 @@ class NavBar extends Component {
   }
 }
 
-export default withRouter(NavBar);
+export default withRouter(NavBar)
