4c00a9ace239fd0053fc9d72ee93aa82b905adc5
Ryan Crowley <ryancrowleycode@gmail.com>
2019-11-26 11:00:39 -0600

    Logout now looks like a link, because it is.

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -6,7 +6,6 @@ class NavBar extends Component {
 
   handleLogout = () => {
     this.props.clearUser()
-    this.props.history.push('/')
   }
 
   render() {
@@ -25,12 +24,11 @@ class NavBar extends Component {
                 <li><Link className="nav-link" to="/animals">Animals</Link></li>
                 <li><Link className="nav-link" to="/employees">Employees</Link></li>
                 <li><Link className="nav-link" to="/owners">Owners</Link></li>
-                <li><span className="nav-link" onClick={this.handleLogout}>Logout</span></li>
+                <li><Link className="nav-link" onClick={this.handleLogout} to="/">Logout</Link></li>
               </React.Fragment>
               :
               <li><Link className="nav-link" to="/login">Login</Link></li>
             }
-
           </ul>
         </nav>
       </header>
